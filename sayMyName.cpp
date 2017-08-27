1cd02d26d11047264afead642d3a1632aaea0269
knewyousir <kevin.birk@gmail.com>
2017-08-27 17:30:32 -0400

    added saymyname program

---
+++ sayMyName.cpp
@@ -0,0 +1,21 @@
+// sayMyName.cpp
+// This file is a test of IO
+// by knewyousir
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main (){
+
+string name;
+cout << "What is your name?\n";
+cin >> name;
+cout << "Your name is " << name << endl;
+
+return 0;
+}
